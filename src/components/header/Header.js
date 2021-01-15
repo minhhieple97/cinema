0a3cfd093750515981283575145108aea3f39cd0
HiepCongChua <hieplevuc@gmail.com>
2021-01-15 21:28:19 +0700

    add header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,50 +1,90 @@
-import React, { useState } from 'react'
-import logo from '../../logo.svg';
-import "./Header.scss";
+import React, { useState } from 'react';
+import logo from '../../assets/cinema-logo.svg';
+import './Header.scss';
+const HEADER_LIST = [
+  {
+    id: 1,
+    iconClass: 'fas fa-film',
+    name: 'Now Playing',
+    type: 'now_playing'
+  },
+  {
+    id: 2,
+    iconClass: 'fas fa-fire',
+    name: 'Popular',
+    type: 'popular'
+  },
+  {
+    id: 3,
+    iconClass: 'fas fa-star',
+    name: 'Top Rated',
+    type: 'top_rated'
+  },
+  {
+    id: 4,
+    iconClass: 'fas fa-plus-square',
+    name: 'Upcoming',
+    type: 'upcoming'
+  }
+];
 const Header = () => {
-    const [navClass, setNavClass] = useState(false);
-    const [menuClass, setMenuClass] = useState(false);
-    const toggleMenu = () => {
-        setNavClass(!navClass);
-        setMenuClass(!menuClass);
-        if (navClass) {
-            document.body.classList.add('header-nav-open');
-        }
-        else {
-            document.body.classList.remove('header-nav-open')
-        }
+  const [navClass, setNavClass] = useState(false);
+  const [menuClass, setMenuClass] = useState(false);
+  const toggleMenu = () => {
+    setNavClass(!navClass);
+    setMenuClass(!menuClass);
+    if (navClass) {
+      document.body.classList.add('header-nav-open');
+    } else {
+      document.body.classList.remove('header-nav-open');
     }
-    return (
-        <>
-            <div className="header-nav-wrapper" >
-                <div className="header-bar" ></div>
-                <div className="header-navbar" >
-                    <div className="header-image" >
-                        {/* <img src={logo} alt="" /> */}
-                       Cinema App
-                    </div>
-                    <div
-                        className={`${menuClass ? "header-menu-toggle is-active" : "header-menu-toggle"}`}
-                        id="header-mobile-menu"
-                        onClick={() => toggleMenu()}
-                    >
-                        <span className="bar" ></span>
-                        <span className="bar" ></span>
-                        <span className="bar" ></span>
-                    </div>
-                    <ul className={`${navClass ? "header-nav header-mobile-nav" : "header-nav"}`} >
-                        <li className="header-nav-item" >Now playing</li>
-                        <li className="header-nav-item" >New Movies</li>
-                        <input
-                            className="search-input"
-                            type="text"
-                            placeholder="Search for a movie"
-                        />
-                    </ul>
-                </div>
-            </div>
-        </>
-    )
-}
+  };
+  return (
+    <>
+      <div className="header-nav-wrapper">
+        <div className="header-bar"></div>
+        <div className="header-navbar">
+          <div className="header-image">
+            <img src={logo} alt="" />
+          </div>
+          <div
+            className={`${
+              menuClass ? 'header-menu-toggle is-active' : 'header-menu-toggle'
+            }`}
+            id="header-mobile-menu"
+            onClick={() => toggleMenu()}
+          >
+            <span className="bar"></span>
+            <span className="bar"></span>
+            <span className="bar"></span>
+          </div>
+          <ul
+            className={`${
+              navClass ? 'header-nav header-mobile-nav' : 'header-nav'
+            }`}
+          >
+            {HEADER_LIST.map((el) => {
+              return (
+                <li key={el.id} className="header-nav-item">
+                  <span className="header-list-name">
+                    <i className={el.iconClass}></i>
+                  </span>
+                  &nbsp;
+                  <span className="header-list-name">{el.name}</span>
+                </li>
+              );
+            })}
 
-export default Header
+            <input
+              className="search-input"
+              type="text"
+              placeholder="Search for a movie"
+            />
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
