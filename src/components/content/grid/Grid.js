a8406557eb29fbf95bf5a803bc8e64507403c827
HiepCongChua <hieplevuc@gmail.com>
2021-01-21 22:04:04 +0700

    update

--- src/components/content/grid/Grid.js
+++ src/components/content/grid/Grid.js
@@ -1,4 +1,5 @@
 import { IMAGE_URL } from '../../../services/movie';
+import LazyImage from '../../lazy-image/LazyImage';
 import Rating from '../rating/Rating';
 import './Grid.scss';
 const Grid = ({ images }) => {
@@ -8,9 +9,11 @@ const Grid = ({ images }) => {
         {images.map((image, i) => {
           return (
             <div key={image.id}>
-              <div
+              
+              <LazyImage
                 className="grid-cell"
-                style={{ backgroundImage: `url(${IMAGE_URL}/${image.poster_path})` }}
+                src={`url(${IMAGE_URL}/${image.poster_path})`}
+                alt="placeholder"
               >
                 <div className="grid-read-more">
                   <button className="grid-cell-button">Read More</button>
@@ -18,12 +21,17 @@ const Grid = ({ images }) => {
                 <div className="grid-detail">
                   <span className="grid-detail-title">{image.title}</span>
                   <div className="grid-detail-rating">
-                    <Rating rating={image.vote_average} totalStars={10}></Rating>
+                    <Rating
+                      rating={image.vote_average}
+                      totalStars={10}
+                    ></Rating>
                     &nbsp;&nbsp;
-                    <div className="grid-vote-average">{image.vote_average}</div>
+                    <div className="grid-vote-average">
+                      {image.vote_average}
+                    </div>
                   </div>
                 </div>
-              </div>
+              </LazyImage>
             </div>
           );
         })}
--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,48 +1,48 @@
 import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { loadMoreMovies, setResponsePageNumber } from '../../redux/actions/movies';
+import './Main.scss';
+import {
+  loadMoreMovies,
+  setResponsePageNumber
+} from '../../redux/actions/movies';
 import MainContent from '../content/main-content/MainContent';
 import Spinner from '../spinner/Spinner';
 const Main = () => {
-  const { loading, page, totalPages, list } = useSelector(state => state.movies)
+  const { loading, page, totalPages, list } = useSelector(
+    (state) => state.movies
+  );
   const [currentPage, setCurrentPage] = useState(page);
   const mainRef = useRef();
   const bottomLineRef = useRef();
   const dispatch = useDispatch();
-  console.log({ currentPage, list: list.length })
   useEffect(() => {
-    if (list.length > 1 && totalPages > 0) {
-      console.log("renderrrrrrr...")
-      dispatch(loadMoreMovies('now_playing', currentPage))
-      dispatch(setResponsePageNumber(currentPage, totalPages))
-    }
+    dispatch(setResponsePageNumber(currentPage, totalPages));
     // eslint-disable-next-line
-  }, [currentPage, totalPages])
-  const fetchData = useCallback(
-    () => {
-      if (page < totalPages) {
-        setCurrentPage(prev => prev + 1)
-      }
-    },
-    [page, totalPages],
-  )
-  const handleScroll = () => {
-
-    const containerHeight = mainRef.current.getBoundingClientRect().height;
-    const { top: bottomLineTop } = bottomLineRef.current.getBoundingClientRect();
-    console.log({ containerHeight })
-    if (bottomLineTop <= containerHeight) {
-      fetchData()
+  }, [currentPage, totalPages]);
+  const fetchData = useCallback(() => {
+    if (page < totalPages) {
+      setCurrentPage((prev) => {
+        dispatch(loadMoreMovies('now_playing', prev + 1));
+        return prev + 1;
+      });
     }
-  }
+  }, [page, totalPages, dispatch]);
+  const handleScroll = () => {
+    const containerHeight = mainRef.current.getBoundingClientRect().height;
+    const {
+      top: bottomLineTop
+    } = bottomLineRef.current.getBoundingClientRect();
+    if (bottomLineTop <= containerHeight) {
+      fetchData();
+    }
+  };
   return (
     <>
-      <div className="main" ref={mainRef} onScroll={handleScroll} >
+      <div className="main" ref={mainRef} onScroll={handleScroll}>
         {loading ? <Spinner></Spinner> : <MainContent></MainContent>}
-        <div ref={bottomLineRef} ></div>
+        <div ref={bottomLineRef}></div>
       </div>
     </>
-
   );
 };
 export default Main;
